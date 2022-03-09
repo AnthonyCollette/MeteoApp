a3dec08f3985954f84de9441c2934047361ed831
Anthony Collette <100pour100inde@gmail.com>
2022-03-09 16:44:31 +0100

    application terminée ( desktop only )

---
+++ src/components/card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const card = (props) => {
+	console.log(props.city)
+	return (
+		<div className="card">
+			<img
+				src={`http://openweathermap.org/img/wn/${props.city.icon}@4x.png`}
+				alt="Icone de météo"
+			/>
+			<div className="right-column">
+				<p className="temperature">{props.city.temp}°C</p>
+				<p>{props.city.desc}</p>
+			</div>
+		</div>
+	)
+}
+
+export default card
---
+++ src/components/hero.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import video from '../styles/assets/video/clouds.mp4'
+import Card from './card'
+
+const Hero = () => {
+	const [city, setCity] = useState('')
+	const apiKey = 'e1cfcb68dedfb5eecbf214bf2b358f23'
+	let lat = ''
+	let lon = ''
+	const [town, setTown] = useState({})
+	// let town = {}
+	const [showCard, setShowCard] = useState(false)
+	const fetchApi = () => {
+		fetch(
+			`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
+		)
+			.then((res) => res.json())
+			.then((result) => {
+				lat = result[0].lat
+				lon = result[0].lon
+				return fetch(
+					`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=fr&appid=${apiKey}`
+				)
+			})
+			.then((data) => data.json())
+			.then((res) => {
+				console.log(res)
+				setTown({
+					temp: Math.trunc(res.main.temp),
+					desc: res.weather[0].description,
+					icon: res.weather[0].icon,
+				})
+
+				console.log(town)
+			})
+			.catch((error) => console.log(error))
+	}
+
+	return (
+		<div className="hero">
+			<video loop muted autoPlay>
+				<source src={video} type="video/mp4" />
+			</video>
+			<div className="overlay">
+				<form
+					onSubmit={(e) => {
+						e.preventDefault()
+						fetchApi()
+						setShowCard(true)
+					}}
+				>
+					<input
+						type="text"
+						placeholder="Quelle ville ?"
+						onChange={(e) => setCity(e.target.value)}
+					/>
+				</form>
+				{showCard ? <Card city={town} /> : ''}
+			</div>
+		</div>
+	)
+}
+
+export default Hero
